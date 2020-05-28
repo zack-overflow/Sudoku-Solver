b38da69c8690020dd6b4c3b413e69272218893d5
Stuart M. Hayes 21 <shayes2021@moose.cs.dartmouth.edu>
2020-05-28 18:14:48 -0400

    added array of possibilites for each cell in creator to limit options for random selection

--- creator/creator.c
+++ creator/creator.c
@@ -13,8 +13,9 @@
 
 #define ROW 9
 #define COL 9
+#define N 9
 
-int checkMove(int board[ROW][COL], int row, int col, int value);
+void checkMove(int board[ROW][COL], int row, int col, int *listofnums);
 void boardFill(int board[ROW][COL]);
 int checkBoard(int board[ROW][COL], int row, int col, int value);
 
@@ -24,25 +25,62 @@ int checkBoard(int board[ROW][COL], int row, int col, int value){
     return 1;
 }
 
-int checkMove(int board[ROW][COL], int row, int col, int value){
+void checkMove(int board[ROW][COL], int row, int col, int *listofnums){
     int i, j;
     int rowmod;
     int rowstart;
     int colmod;
     int colstart;
+    int index;
+    int numstaken[N+1] = {0};
+
+    for(i = 0; i <= 9; i++){
+        listofnums[i] = 0;
+    }
+
 
     //check column
     for(i = 0; i < 9; i++){
-        if(board[row][i] == value){
-            return 0;
+        index = board[row][i];
+        numstaken[index] = index;
+    }
+    for(i = 0; i <= 9; i++){
+        if(numstaken[i] == 0){
+            listofnums[i] = i;
         }
     }
+    fprintf(stdout, "column list: ");
+    for(i = 0; i <= 9; i++){
+        fprintf(stdout, "%d ", listofnums[i]);
+    }
+    fprintf(stdout, "\n");
+
+    //reset numstaken
+    for(i = 0; i <= 9; i++){
+        numstaken[i] = 0;
+    }
+
 
     //check row
     for(i = 0; i < 9; i++){
-        if(board[i][col] == value){
-            return 0;
+        index = board[i][col];
+        numstaken[index] = index;
+    }
+    fprintf(stdout, "check\n");
+    for(i = 0; i <= 9; i++){
+        if(listofnums[i] == numstaken[i]){
+            listofnums[i] = 0;
         }
+    }
+    fprintf(stdout, "row list: ");
+    for(i = 0; i <= 9; i++){
+        fprintf(stdout, "%d ", listofnums[i]);
+    }
+    fprintf(stdout, "\n");
+
+    //reset numstaken
+    for(i = 0; i <= 9; i++){
+        numstaken[i] = 0;
     }
 
     //check box
@@ -54,41 +92,69 @@ int checkMove(int board[ROW][COL], int row, int col, int value){
 
     for(i = 0; i < 3; i++){
         for(j = 0; j < 3; j++){
-            if(board[rowstart + i][colstart + j] == value){
-                return 0;
-            }
+            index = board[rowstart + i][colstart + j];
+            numstaken[index] = index;
         }
     }
-
-    return 1;
+    for(i = 0; i <= 9; i++){
+        if(listofnums[i] == numstaken[i]){
+            listofnums[i] = 0;
+        }
+    }
+    fprintf(stdout, "box list: ");
+    for(i = 0; i <= 9; i++){
+        fprintf(stdout, "%d ", listofnums[i]);
+    }
+    fprintf(stdout, "\n");
 }
 
 
 void boardFill(int board[ROW][COL]){
     int i;
     int j;
-    int valid;
+    int k;
+    int count = 0;
     srand(time(NULL));   // Initialization, should only be called once.
     int randomnum;
+    int listofnums[N+1] = {0};
+    int listofoptions[N+1] = {0};
 
 
     for (i = 0; i < 9; i++){
         for(j = 0; j < 9; j++){
-            //fprintf(stdout, "position: %d %d \n", i, j);
-            randomnum = rand() % 10;
-            if(randomnum < 5){
-                board[i][j] = 0;
+            fprintf(stdout, "position: %d %d\n", i, j);
+
+            for(k = 0; k <= 9; k++){
+                listofoptions[k] = 0;
             }
-            else{
-                randomnum = rand() % 10;
-                valid = checkMove(board, i, j, randomnum);
-                //fprintf(stdout, "valid move\n");
-                while(valid != 1){
-                    randomnum = rand() % 10;
-                    valid = checkMove(board, i, j, randomnum);
+
+
+            checkMove(board, i, j, listofnums);
+            fprintf(stdout, "list of options: ");
+            for(k = 0; k <= 9; k++){
+                fprintf(stdout, "%d ", listofnums[k]);
+            }
+            fprintf(stdout, "\n");
+
+            for(k = 0; k <= 9; k++){
+                if(listofnums[k] != 0){
+                    listofoptions[count] = listofnums[k];
+                    count++;
                 }
-                board[i][j] = randomnum;
             }
+            fprintf(stdout, "final options: ");
+            for(k = 0; k < count; k++){
+                fprintf(stdout, "%d ", listofoptions[k]);
+            }
+            fprintf(stdout, "\n");
+
+            randomnum = rand() % count;
+            board[i][j] = listofoptions[randomnum];
+            fprintf(stdout, "number chosen: %d\n\n", board[i][j]);
+            count = 0;
+        }
+        if(i == 2){
+            break;
         }
     }
 
